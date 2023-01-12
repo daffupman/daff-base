bea16b00d6fb620d8c95cd3fd984dd6b2e45ce85
daff <wzjin85@foxmail.com>
2023-01-12 12:47:42 +0800

    优化BizDataLoader接口

--- cache/src/main/java/io/daff/cache/BizDataLoader.java
+++ cache/src/main/java/io/daff/cache/BizDataLoader.java
@@ -1,15 +1,20 @@
 package io.daff.cache;
 
 /**
- * 业务数据模型预加载接口
+ * 业务数据模型预加载器，此loader是常驻JVM的缓存，在项目启动的时候启动
  *
  * @author daff
- * @since 2022/3/20
+ * @since 2021/11/15
  */
 public interface BizDataLoader {
 
     /**
-     * 加载业务数据模型
+     * 加载业务数据
      */
     void load();
+
+    /**
+     * 判断数据加载是否完成
+     */
+    boolean finish();
 }
